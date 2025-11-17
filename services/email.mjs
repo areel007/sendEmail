b8d7596d16d20cbab6ffd87a1e4d23281a1ac9b7
bamidele <morenikejisunday12@gmail.com>
2025-11-17 17:48:12 +0100

    fixes

--- services/email.mjs
+++ services/email.mjs
@@ -1,12 +1,11 @@
 import { Resend } from "resend";
 
-const resend = new Resend("re_L95YvJDj_Eduvya8v8PzWWGd2N27scxDZ`");
+const resend = new Resend("re_L95YvJDj_Eduvya8v8PzWWGd2N27scxDZ");
 
-export const sendEmail = (otp, email) => {
-  return resend.emails.send({
+export const sendEmail = async (otp, email) =>
+  await resend.emails.send({
     from: "onboarding@resend.dev",
     to: email,
     subject: "OTP Verification",
-    html: `<p>Your OTP is: ${otp}</p>`,
+    html: `<strong>Your OTP is: ${otp}</strong>`,
   });
-};
